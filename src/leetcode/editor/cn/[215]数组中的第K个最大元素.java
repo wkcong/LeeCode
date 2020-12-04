606b7287e2189574ab35f4b865d134c529c21cf4
CharmWang <738402018@qq.com>
2020-12-05 00:12:36 +0800

    add quickSort for 215 ---divide-and-conquer

--- src/leetcode/editor/cn/[215]数组中的第K个最大元素.java
+++ src/leetcode/editor/cn/[215]数组中的第K个最大元素.java
@@ -23,9 +23,40 @@ import java.util.Arrays;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        //1.用快速排序的思想来做
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        //1.用快速排序的思想来做，sort方法就是利用快排来做的
+        /*Arrays.sort(nums);
+        return nums[nums.length-k];*/
+        //2.快排的简化
+        //我们只需要得到位置就行了，不需要关心数组是不是有序
+        return quickSort(nums,0,nums.length-1,nums.length-k);
+    }
+    public int quickSort(int nums[],int left,int right,int k){
+        int p = partition(nums,left,right);
+        if(p==k){
+            return nums[p];
+        }else {
+            //当p>k，就只用计算左边的数组；反之就计算右边的数组
+            return p>k?quickSort(nums,left,p-1,k):quickSort(nums,p+1,right,k);
+        }
+    }
+
+    public int partition(int nums[],int left,int right){
+        //标准的快排方法
+        int i=left;
+        int j=right;
+        int temp = nums[left];
+        while(i<j){
+            while(i<j && nums[j]>temp){
+                j--;
+            }
+            nums[i]=nums[j];
+            while(i<j && nums[i]<=temp){
+                i++;
+            }
+            nums[j]=nums[i];
+        }
+        nums[i]=temp;
+        return i;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
