24296ad854ba533f16a836a13302c1b652403994
CharmWang <738402018@qq.com>
2020-12-06 15:18:40 +0800

    48 旋转图像

---
+++ src/leetcode/editor/cn/[48]旋转图像.java
@@ -0,0 +1,71 @@
+//给定一个 n × n 的二维矩阵表示一个图像。 
+//
+// 将图像顺时针旋转 90 度。 
+//
+// 说明： 
+//
+// 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。 
+//
+// 示例 1: 
+//
+// 给定 matrix = 
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+// 
+//
+// 示例 2: 
+//
+// 给定 matrix =
+//[
+//  [ 5, 1, 9,11],
+//  [ 2, 4, 8,10],
+//  [13, 3, 6, 7],
+//  [15,14,12,16]
+//], 
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [15,13, 2, 5],
+//  [14, 3, 4, 1],
+//  [12, 6, 8, 9],
+//  [16, 7,10,11]
+//]
+// 
+// Related Topics 数组 
+// 👍 647 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void rotate(int[][] matrix) {
+        //可以先转置，再反转
+        //转置 a[i][j]=a[j][i]
+        for(int i=0;i<matrix.length;i++){
+            //j从i开始,对角线不用改,但是从i+1开始的话数组会溢出
+            for(int j=i;j<matrix.length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //反转：将第一列与n-1列交换
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1]=temp;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
